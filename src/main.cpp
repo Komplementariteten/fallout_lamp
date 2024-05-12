41313c0cf8245b82b19836b3a28cafe53153c810
Komplementariteten <47053492+Komplementariteten@users.norepley.github.com>
2024-05-12 10:51:42 +0200

    Init

---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by me on 12.05.24.
+//
+
+#include "main.h"
---
+++ src/main.h
@@ -0,0 +1,14 @@
+//
+// Created by me on 12.05.24.
+//
+
+#ifndef FALLOUT_LAMP_MAIN_H
+#define FALLOUT_LAMP_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //FALLOUT_LAMP_MAIN_H

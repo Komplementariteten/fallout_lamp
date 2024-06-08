6f4e066315cc5bce12a126dfdbcd1ccbfd42b17c
Komplementariteten <47053492+Komplementariteten@users.norepley.github.com>
2024-06-08 15:21:54 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -3,3 +3,67 @@
 //
 
 #include "main.h"
+
+
+int main() {
+    stdio_init_all();
+    if (cyw43_arch_init()) {
+        printf("Wi-Fi init failed");
+        return -1;
+    }
+
+    while (true) {
+        blink_board_led();
+    }
+    // report_board_status();
+    // auto state = new rgb(false, false, false);
+}
+
+void blink_board_led() {
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+    sleep_ms(250);
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+    sleep_ms(250);
+}
+
+
+void toogle_white(rgb* state){
+    toogle_red(state);
+    toogle_green(state);
+    toogle_blue(state);
+}
+
+void report_board_status() {
+
+    const uint BOARD_LED = 25;
+    gpio_init(BOARD_LED);
+    gpio_set_dir(BOARD_LED, GPIO_OUT);
+    gpio_put(BOARD_LED, true);
+
+}
+
+void toogle_red(rgb* state){
+    state->red = !state->red;
+}
+
+void toogle_green(rgb* state){
+    state->green = !state->green;
+}
+void toogle_blue(rgb* state){
+    state->blue = !state->blue;
+}
+
+
+void init_gpio(rgb* state) {
+    gpio_init(RED_GPIO);
+    gpio_set_dir(RED_GPIO, GPIO_OUT);
+    gpio_put(RED_GPIO, state->red);
+
+    gpio_init(BLUE_GPIO);
+    gpio_set_dir(BLUE_GPIO, GPIO_OUT);
+    gpio_put(BLUE_GPIO, state->blue);
+
+    gpio_init(GREEN_GPIO);
+    gpio_set_dir(GREEN_GPIO, GPIO_OUT);
+    gpio_put(GREEN_GPIO, state->green);
+}
--- src/main.h
+++ src/main.h
@@ -5,10 +5,50 @@
 #ifndef FALLOUT_LAMP_MAIN_H
 #define FALLOUT_LAMP_MAIN_H
 
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "pico/cyw43_arch.h"
+
+/*
+#include "btstack.h"
+#include "server_common.h"
+*/
+
+#define RED_GPIO 19
+#define GREEN_GPIO 20
+#define BLUE_GPIO 21
 
 class main {
 
 };
+
+
+class rgb
+{
+private:
+    /* data */
+public:
+    rgb
+(bool r, bool g, bool b): red(r), green(g), blue(b){};
+bool red;
+bool green;
+bool blue;
+};
+
+void blink_board_led();
+
+void init_gpio(rgb* state);
+
+void toogle_white(rgb* state);
+
+void toogle_red(rgb* state);
+
+void toogle_green(rgb* state);
+
+void toogle_blue(rgb* state);
+
+void report_board_status();
 
 
 #endif //FALLOUT_LAMP_MAIN_H
